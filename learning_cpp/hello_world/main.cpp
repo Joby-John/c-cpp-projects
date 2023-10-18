ce29506cf4aa9e4453f4f158a427b4f9ada529c0
Joby-John <joby432john@gmail.com>
2023-10-18 08:06:47 +0530

    learned till if else

--- learning_cpp/hello_world/main.cpp
+++ learning_cpp/hello_world/main.cpp
@@ -2,7 +2,39 @@
 
 using namespace std;
 
-int main(){
-cout<<"Hello world";
+int main(){ 
+//     string intro;
+//     int marksInMaths, a= 5, b = 7;
+//     cout<<"Enter your name"<<endl;
+//     cin>>intro;
+//     cout<<"Enter your score"<<endl;
+//     cin>>marksInMaths;
+
+// cout<<"Hi "<<intro<<endl<<"Score is: "<<marksInMaths;
+
+// cout<<"Lets test it:";
+// cout<<"Whats "<<a<<"+"<<b<<":"<<a+b<<endl;
+// cout<<"Whats "<<a<<"-"<<b<<":"<<a-b<<endl;
+// cout<<"Whats "<<a<<"x"<<b<<":"<<a*b<<endl;
+// cout<<"Whats "<<a<<"/"<<b<<":"<<a/b<<endl;
+// cout<<"Whats "<<a<<"%"<<b<<":"<<a%b<<endl;
+
+//conditionals
+int age;
+cout<<"Enter your age"<<endl;
+cin>>age;
+if (age>150)
+{
+    cout<<"Invalid age";
+}
+else if (age>=18)
+{
+    cout<<"You can vote";
+}
+else
+{
+    cout<<"You cannot vote";
+}
+
 return 0;
 }
