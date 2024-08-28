6e1f398f0bae5fefdeaa575b171eb3e8dd0785c3
Joby-John <joby432john@gmail.com>
2024-08-28 20:50:20 +0530

    done the design of file

---
+++ fileManager/main.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<fstream>
+#include<windows.h>
+#include<string>
+
+using namespace std;
+
+class listFile
+{
+public:
+    void listFiles(const string& path)
+    {
+
+    }
+};
+
+class Write
+{
+public:
+    void create()
+    {
+
+    }
+
+    void append( const string& fName)
+    {
+
+    }
+};
+
+class Read
+{
+    public:
+        void read(const string& fName)
+        {
+
+        }
+
+};
+
+class Delete
+{
+    public:
+        void del(const string& fname)
+        {
+
+        }
+};
+
+class vControl
+{
+    void init()
+    {
+
+    }
+
+    void status()
+    {
+
+    }
+
+    void add()
+    {
+        //add
+    }
+
+    void commit()
+    {
+        //commit
+    }
+
+    void displayLog()
+    {
+        //display commit log with hash
+    }
+
+    void revert(string commitHash)
+    {
+
+    }
+
+    
+};
+
+
+int main()
+{
+    return 0;
+}
