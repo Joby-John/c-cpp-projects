ad5a6ed4dbe3a740422ff8b81ab4a293a4327daf
Joby-John <joby432john@gmail.com>
2024-08-31 00:03:34 +0530

    did the main menu using chat gpt, need additions

--- fileManager/main.cpp
+++ fileManager/main.cpp
@@ -30,6 +30,8 @@ public:
         {
             cerr <<e.what() << '\n';
         }
+
+        system("pause");
         
     }
 
@@ -104,6 +106,7 @@ class Delete
 
 class vControl
 {
+public:
     void init()
     {
 
@@ -137,15 +140,99 @@ class vControl
     
 };
 
+void mainMenu()
+{
+        int choice;
+
+    Write writeObj;
+    listFile listObj;
+    Read readObj;
+    Delete deleteObj;
+    vControl vcObj;
+
+    while (true) {
+        system("CLS");
+        cout << "\nMain Menu\n";
+        cout << "1. List Files\n";
+        cout << "2. Create File\n";
+        cout << "3. Read File\n";
+        cout << "4. Append to File\n";
+        cout << "5. Delete File\n";
+        cout << "6. Initialize Version Control\n";
+        cout << "7. Check Version Control Status\n";
+        cout << "8. Add File to Version Control\n";
+        cout << "9. Commit Changes\n";
+        cout << "10. Display Commit Log\n";
+        cout << "11. Revert to Previous Commit\n";
+        cout << "12. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                system("CLS");
+                listObj.listFiles();
+                break;
+            case 2:
+                writeObj.create();
+                break;
+            case 3: {
+                string fileName;
+                cout << "Enter the file name to read: ";
+                cin >> fileName;
+                readObj.read(fileName);
+                break;
+            }
+            case 4: {
+                string fileName;
+                cout << "Enter the file name to append: ";
+                cin >> fileName;
+                writeObj.append(fileName);
+                break;
+            }
+            case 5: {
+                string fileName;
+                cout << "Enter the file name to delete: ";
+                cin >> fileName;
+                deleteObj.del(fileName);
+                break;
+            }
+            case 6:
+                vcObj.init();
+                break;
+            case 7:
+                vcObj.status();
+                break;
+            case 8:
+                vcObj.add();
+                break;
+            case 9:
+                vcObj.commit();
+                break;
+            case 10:
+                vcObj.displayLog();
+                break;
+            case 11: {
+                string commitHash;
+                cout << "Enter the commit hash to revert to: ";
+                cin >> commitHash;
+                vcObj.revert(commitHash);
+                break;
+            }
+            case 12:
+                cout << "Exiting..." << endl;
+                return;
+            default:
+                cout << "Invalid choice, please try again." << endl;
+        }
+    }
+}
+
 
 int main()
 {
     system("CLS");
-    Write firstFile;
-    listFile listfile;
-
-    listfile.listFiles();
-
-    //firstFile.create();
+    
+    mainMenu();
     return 0;
 }
